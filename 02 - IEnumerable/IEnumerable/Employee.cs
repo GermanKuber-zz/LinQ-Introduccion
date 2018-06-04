74a28191ec1145366663dda97d3cf6f28773151b
Germán Küber <german.kuber@outlook.com>
2018-06-03 22:27:50 -0300

    Added IEnumerable Example

---
+++ 02 - IEnumerable/IEnumerable/Employee.cs
@@ -0,0 +1,8 @@
+﻿namespace IEnumerable.Core
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ 02 - IEnumerable/IEnumerable/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IEnumerable.Core
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<Employee> developersList = new List<Employee>
+            {
+                new Employee { Id = 1, Name= "Dario" },
+                new Employee { Id = 2, Name= "Valentina" },
+                new Employee { Id = 2, Name= "Ailin" }
+            };
+
+            Employee[] developersArray = new Employee[]
+            {
+                new Employee { Id = 1, Name= "Dario" },
+                new Employee { Id = 2, Name= "Valentina" },
+                new Employee { Id = 2, Name= "Ailin" }
+            };
+
+
+
+
+            //foreach (var employee in developersList)
+            //    Console.WriteLine(employee.Name);
+
+
+            //IEnumerator<Employee> enumerator = developersArray.GetEnumerator();
+
+            //while (enumerator.MoveNext())
+            //{
+            //    Console.WriteLine(enumerator.Current.Name);
+            //}
+
+            Console.ReadKey();
+        }
+    }
+}
